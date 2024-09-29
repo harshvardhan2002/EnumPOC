27d1e6f2c46d1d6449e326150dbc0d3972fcde3c
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-30 02:23:50 +0530

    Update Program.cs
    Avoiding magic number.

--- EnumPOC/Program.cs
+++ EnumPOC/Program.cs
@@ -2,14 +2,16 @@
 {
     internal class Program
     {
+        const int min_range = 0;
+        const int max_range = 6;
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the day it was yesterday (in form of 0-6 according to ascending order): ");
             int day=Convert.ToInt32(Console.ReadLine());
 
-            if (day >= 0 && day <= 6)
+            if (day >= min_range && day <= max_range)
             {
-                int todayIndex = (day + 1) % 7;
+                int todayIndex = (day + (min_range+1)) % (max_range+1);
                 Days today = (Days)todayIndex;
                 Console.WriteLine("Today is: " + today);
             }
