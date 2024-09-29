aacb0751609ea2b5837b69416b54915ffb8509a6
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-29 23:56:51 +0530

    Add files via upload

---
+++ EnumPOC/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EnumPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the day it was yesterday (in form of 0-6 according to ascending order): ");
+            int day=Convert.ToInt32(Console.ReadLine());
+
+            if (day >= 0 && day <= 6)
+            {
+                int todayIndex = (day + 1) % 7;
+                Days today = (Days)todayIndex;
+                Console.WriteLine("Today is: " + today);
+            }
+            else Console.WriteLine("Enter valid number");
+        }
+    }
+    enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday, 
+        Thursday,
+        Friday,
+        Saturday, 
+        Sunday
+    }
+}
