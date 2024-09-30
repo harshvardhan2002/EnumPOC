50a25adad9d34e68c8f92b582e594074ff0c349c
Harshvardhan Gupta <76556011+harshvardhan2002@users.noreply.github.com>
2024-09-30 10:54:55 +0530

    Update Program.cs

--- EnumPOC/Program.cs
+++ EnumPOC/Program.cs
@@ -2,16 +2,16 @@
 {
     internal class Program
     {
-        const int min_range = 0;
-        const int max_range = 6;
+        const int MIN_RANGE = 0;
+        const int MAX_RANGE = 6;
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the day it was yesterday (in form of 0-6 according to ascending order): ");
             int day=Convert.ToInt32(Console.ReadLine());
 
-            if (day >= min_range && day <= max_range)
+            if (day >= MIN_RANGE && day <= MAX_RANGE)
             {
-                int todayIndex = (day + (min_range+1)) % (max_range+1);
+                int todayIndex = (day + (MIN_RANGE+1)) % (MAX_RANGE+1);
                 Days today = (Days)todayIndex;
                 Console.WriteLine("Today is: " + today);
             }
